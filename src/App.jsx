54c72066c4a14ca8db84f257c7120336b85e4448
saba <marghanias@gmail.com>
2024-06-21 20:40:49 +0400

    project initialize

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// https://api.ipbase.com/v2/info?apikey=ipb_live_XPsrYmD0Hfwe319OfuuM4DJd9bVD5zty8fa8ucJr&language=en&ip=109.172.197.23
+import Main from './components/Main';
+
+function App() {
+  return (
+    <>
+      <Main></Main>
+    </>
+  );
+}
+
+export default App;
