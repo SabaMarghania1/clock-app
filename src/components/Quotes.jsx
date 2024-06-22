d866cdf833ecad3f8a83af4482d64688f089293c
saba <marghanias@gmail.com>
2024-06-22 16:26:55 +0400

    changed quote api

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -4,10 +4,11 @@ import icon from '../assets/shared/icon-refresh.svg';
 
 const Quotes = () => {
   const [quote, setQuote] = useState([]);
+  console.log(quote);
 
   const fetchQuotes = async () => {
     try {
-      const response = await fetch('https://dummyjson.com/quotes/random');
+      const response = await fetch('https://api.quotable.io/random');
       const data = await response.json();
       setQuote(data);
     } catch (error) {
@@ -23,7 +24,7 @@ const Quotes = () => {
     <section className="quote-container">
       {quote && (
         <>
-          <p className="quote">“{quote.quote}”</p>
+          <p className="quote">“{quote.content}”</p>
           <span className="author">{quote.author}</span>
         </>
       )}
